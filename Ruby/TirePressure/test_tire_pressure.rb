2af0142f14186e1284e88360f532a37e923732d6
fiona.man <fiona.man@rea-group.com>
2016-08-26 10:37:03 +1000

    decouple Sensor and Alarm.

--- Ruby/TirePressure/test_tire_pressure.rb
+++ Ruby/TirePressure/test_tire_pressure.rb
@@ -1,11 +1,28 @@
 require "test/unit"
 
-require 'tire_pressure'
+require_relative 'tire_pressure'
 
 class TestAlarm < Test::Unit::TestCase
- 
+
   def test_alarm_off_by_default
     assert(!Alarm.new().is_alarm_on)
   end
- 
+
+  def test_alarm_on_when_lower_than_low_boundary
+    alarm = Alarm.new()
+    alarm.check(15)
+    assert(alarm.is_alarm_on)
+  end
+
+  def test_alarm_on_when_higher_than_high_boundary
+    alarm = Alarm.new()
+    alarm.check(22)
+    assert(alarm.is_alarm_on)
+  end
+
+  def test_alarm_off_when_pressure_is_normal
+    alarm = Alarm.new()
+    alarm.check(20)
+    assert(!alarm.is_alarm_on)
+  end
 end
